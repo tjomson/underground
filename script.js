4d74e4dc729643d8461fbb6dade6b77f536e32ba
Unknown <thomaskilbak@gmail.com>
2018-04-04 09:28:48 +0200

    2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function passWord() {
 var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
+var pass1 = prompt('Kodeord pls',' ');
 while (testV < 3) {
 if (!pass1) 
 history.go(-1);
@@ -11,7 +11,7 @@ break;
 } 
 testV+=1;
 var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+prompt('Lmao du er ikke underground nok','Password');
 }
 if (pass1.toLowerCase()!="password" & testV ==3) 
 history.go(-1);
