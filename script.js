8a293ee49867839cfd69f1e86b171c196bd56b57
Unknown <thomaskilbak@gmail.com>
2018-04-04 09:20:54 +0200

    1

---
+++ script.js
@@ -0,0 +1,19 @@
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Your Password',' ');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "123") {
+alert('Du er en god dreng');
+window.open('23r97fh308fb38yfbv308hgv4gfh8ervbe8rgvbq087gf80qy3gvb8qo3g48ovgq3b89o3qg74vb8q080q3v4ob8obq48obf87q3gvb8qyv8.html');
+break;
+} 
+testV+=1;
+var pass1 = 
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+history.go(-1);
+return " ";
+} 
