7b54fb72b146cff8f3df7aca7dbdb745d19cf33c
Unknown <thomaskilbak@gmail.com>
2018-04-04 13:18:12 +0200

    8

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var pass1 = prompt('Kodeord pls',' ');
 while (testV < 3) {
 if (!pass1) 
 history.go(-1);
-if (pass1.toLowerCase() == "123") {
+if (pass1.toLowerCase() == "larsabekat") {
 alert('Du er en god dreng');
 window.open('23r97fh308fb38yfbv308hgv4gfh8ervbe8rgvbq087gf80qy3gvb8qo3g48ovgq3b89o3qg74vb8q080q3v4ob8obq48obf87q3gvb8qyv83b5gy34hbqoi3uh4fq2gf37vbiq3u4r75wfeuqwjg4bqi364fuq2i46gvukq3tf4ui62ffvcu2q4fgru34u3g4yuuy34fyk3brvjhsdk.html');
 break;
